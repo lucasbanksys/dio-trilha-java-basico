a3786ff220a834c52426de8d8e2261c274694fc5
lucasbanksys <lucs@live.jp>
2022-11-22 01:38:47 -0300

    atividade de métodos

---
+++ metodos/src/Usuario.java
@@ -0,0 +1,16 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal da TV: " + smartTv.canal);
+        System.out.println("Volume da TV: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status TV ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status TV ligada? " + smartTv.ligada);
+    }
+}
